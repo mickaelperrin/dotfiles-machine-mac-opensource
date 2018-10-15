b90024267534575c4f6143d4e3100a8cda8c712d
Mickaël PERRIN <dev@mickaelperrin.fr>
2018-10-15 13:26:19 +0200

    [FEATURE] Manage update of existing base files

--- autoinstall.sh
+++ autoinstall.sh
@@ -3,6 +3,10 @@ set -e
 
 DOTFILES_LOCATION=${DOTFILES_LOCATION:-~/.dotfiles.base}
 
-mkdir -p $DOTFILES_LOCATION
-git clone https://github.com/mickaelperrin/dotfiles-machine-mac-opensource $DOTFILES_LOCATION
+if  [ -d $DOTFILES_LOCATION -a -d $DOTFILES_LOCATION/.git ]; then
+  cd $DOTFILES_LOCATION
+  git pull origin master
+else
+  git clone https://github.com/mickaelperrin/dotfiles-machine-mac-opensource $DOTFILES_LOCATION
+fi
 $DOTFILES_LOCATION/install
