2054efb92f881bcebec4252a07c713058536b900
Mickaël PERRIN <dev@mickaelperrin.fr>
2018-10-15 13:22:58 +0200

    [FEATURE] Auto install script

--- autoinstall.sh
+++ autoinstall.sh
@@ -4,4 +4,5 @@ set -e
 DOTFILES_LOCATION=${DOTFILES_LOCATION:-~/.dotfiles.base}
 
 mkdir -p $DOTFILES_LOCATION
-git clone
+git clone https://github.com/mickaelperrin/dotfiles-machine-mac-opensource $DOTFILES_LOCATION
+$DOTFILES_LOCATION/install
