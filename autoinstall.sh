736ce5717b1c71d06ddf6e5ce7f90496d383cdf3
Mickaël PERRIN <dev@mickaelperrin.fr>
2018-10-16 10:06:22 +0200

    [FEATURE] Ensure directory exists before cloning

--- autoinstall.sh
+++ autoinstall.sh
@@ -7,6 +7,7 @@ if  [ -d $DOTFILES_LOCATION -a -d $DOTFILES_LOCATION/.git ]; then
   cd $DOTFILES_LOCATION
   git pull origin master
 else
+  mkdir -p $DOTFILES_LOCATION
   git clone https://github.com/mickaelperrin/dotfiles-machine-mac-opensource $DOTFILES_LOCATION
 fi
 $DOTFILES_LOCATION/install
