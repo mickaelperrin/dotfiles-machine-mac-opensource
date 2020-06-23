829d5681690d0c9d18f9a16e44142437557bc9f1
Mickaël Perrin <dev@mickaelperrin.fr>
2020-06-23 11:22:19 +0200

    [BUGFIX] ZSH hist db

--- zsh/includes/plugins.manual.conf.d/zsh-histdb.sh
+++ zsh/includes/plugins.manual.conf.d/zsh-histdb.sh
@@ -11,7 +11,7 @@ add-zsh-hook precmd histdb-update-outcome
 histdb-fzf-widget() {
   local selected num
   setopt localoptions noglobsubst noposixbuiltins pipefail 2> /dev/null
-  selected=( $(histdb --sep 999 | tac | awk -F'999' '{print $4}' |
+  selected=( $(histdb --host --sep 999 | tac | awk -F'999' '{print $5}' |
     FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" fzf) )
 
   LBUFFER=$selected
