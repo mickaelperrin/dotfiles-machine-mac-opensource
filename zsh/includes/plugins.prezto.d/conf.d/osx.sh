89a5e925dddbd2409f4c1351dda0bc1b1a6315fb
Mickaël Perrin <contact@mickaelperrin.fr>
2021-02-16 23:24:38 +0100

    [FEATURE] Add support for man in dash

--- zsh/includes/plugins.prezto.d/conf.d/osx.sh
+++ zsh/includes/plugins.prezto.d/conf.d/osx.sh
@@ -3,4 +3,19 @@
 #
 
 # Set the keyword used by `mand` to open man pages in Dash.app
-zstyle ':prezto:module:osx:man' dash-keyword 'mand'
+zstyle ':prezto:module:osx:man' dash-keyword 'man'
+
+function mand {
+  if (( $# > 0 )); then
+    zstyle -s ':prezto:module:osx:man' dash-keyword 'dashkw' || dashkw='manpages'
+    open "dash-plugin://keys=$dashkw&query=$1" 2> /dev/null
+    if (( $? != 0 )); then
+      print "$0: Dash is not installed" >&2
+      break
+    fi
+  else
+    print 'What manual page do you want?' >&2
+  fi
+
+  unset dashkw
+}
