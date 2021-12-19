5201d5d334121649de76310a41b8824c872860fc
Mickaël Perrin <contact@mickaelperrin.fr>
2021-12-19 19:42:33 +0100

    [BUGFIX] Ensure that correct brew is loaded relatively to arch

--- zsh/10-zshenv.sh
+++ zsh/10-zshenv.sh
@@ -5,8 +5,8 @@
 #   Sorin Ionescu <sorin.ionescu@gmail.com>
 #
 
-# Ensure that a non-login, non-interactive shell has a defined environment.
-if [[ ( "$SHLVL" -eq 1 && ! -o LOGIN ) && -s "${ZDOTDIR:-$HOME}/.zprofile" ]]; then
-  source "${ZDOTDIR:-$HOME}/.zprofile"
-fi
+# Overwrite default path generated with /usr/libexec/path_helper -s in /etc/profile
+# To remove brew
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin"
 
+source "${ZDOTDIR:-$HOME}/.zprofile"
