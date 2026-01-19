6fad13d29df14479e19a80c65f4374760f397d41
Mickaël Perrin <contact@mickaelperrin.fr>
2026-01-19 14:49:24 +0100

    🔧 fix(zsh): use absolute path for trash command in del alias

--- zsh/includes/alias/1-common/alias.tools.sh
+++ zsh/includes/alias/1-common/alias.tools.sh
@@ -3,5 +3,5 @@ alias dud='du -d 1 -h | sort -h'
 alias duf='du -sh * | sort -h'
 alias k9='kill -9'
 # Force use of rmtrash
-alias del='trash -F'
+alias del='/opt/homebrew/opt/trash/bin/trash -F'
 alias rm="echo Use 'del', or the full path '/bin/rm'"
